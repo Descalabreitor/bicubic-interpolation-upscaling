ea6c14d1dc9e6c50cb20a1ea17a728b97c6b7bbb
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-03-11 09:07:38 +0000

    new_branch creation

--- utils/DataManager.py
+++ utils/DataManager.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 import pandas as pd
 import torch
 import torchvision.transforms as transforms
@@ -15,11 +17,21 @@ class DataManager:
         self.lr_dataset_name = lr_dataset_name
         self.hr_dataset_name = hr_dataset_name
         self.data_points = self.get_data_points()
+        self.lr_data_part_names = [
+            "CLM.tiff",
+            "CLP.tiff",
+            "L2A_data.tiff",
+            "dataMask.tiff",
+            "sunAzimuthAngles.tiff",
+            "sunZenithAngles.tiff",
+            "viewAzimuthMean.tiff",
+            "viewZenithMean.tiff"
+        ]
 
     def get_data_points(self):
         hr_data_points_dir = os.listdir(self.data_folder + "/" + self.hr_dataset_name)
         lr_data_points_dir = os.listdir(self.data_folder + "/" + self.lr_dataset_name)
-        data_points_dir = set(hr_data_points_dir + lr_data_points_dir)
+        data_points_dir = list(set(hr_data_points_dir + lr_data_points_dir))
 
         if len(data_points_dir) != len(hr_data_points_dir):
             raise Exception("Hr and Lr datasets do not match")
@@ -33,6 +45,7 @@ class DataManager:
         data_names = self.data_points['points_name'].sample(n=n_samples)
         hr_images = self.get_hr_images(data_names)
         lr_images = self.get_lr_images(data_names)
+        return hr_images, lr_images
 
     def get_hr_images(self, data_points):
         images = []
@@ -43,14 +56,15 @@ class DataManager:
         return images_tensor
 
     def get_lr_images(self, data_points):
-        images = torch.zeros(len(data_points))
+        images_tensor = torch.zeros((len(data_points)))
         source = "L2A"  # Con el core es fijo aqui pero en el  futuro debe ser interchangeable
         for i, data_point in enumerate(data_points):
-            directory = self.data_folder + "/" + self.lr_dataset_name + "/" + data_point + "/"
-            image_package = []
-            for image_id in range(1, 16):
-                directory = directory + "-" + str(id) + "-" + source + "_data.tiff"
-                image_package.append(tifffile.imread(directory))
-            tensor_package = torch.stack([transforms.ToTensor()(img) for img in image_package])
-            images[i] = tensor_package
-        return images
+            data_directory = self.data_folder + "/" + self.lr_dataset_name + "/" + data_point + "/" + source + "/"
+            image_id = random.randint(1, 8)
+            parts_list = []
+            for data_part_name in self.lr_data_part_names:
+                directory = data_directory + data_point + "-" + str(image_id) + "-" + source + data_part_name
+                parts_list.append(transforms.ToTensor()(tifffile.imread(directory)))
+            image_tensor = torch.stack(parts_list)
+            images_tensor[i] = image_tensor
+        return images_tensor
