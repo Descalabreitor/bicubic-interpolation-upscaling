5c857fd03da4fa5ea5029fe94da67c170e28aa59
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-03-16 12:07:03 +0000

    DataManager.py test and usage

--- utils/DataManager.py
+++ utils/DataManager.py
@@ -1,11 +1,10 @@
 import os
-import random
-
 import pandas as pd
 import torch
 import torchvision.transforms as transforms
 import PIL.Image as Image
 import tifffile
+import numpy as np
 
 
 class DataManager:
@@ -17,34 +16,15 @@ class DataManager:
         self.lr_dataset_name = lr_dataset_name
         self.hr_dataset_name = hr_dataset_name
         self.data_points = self.get_data_points()
-        self.lr_data_part_names = [
-            "CLM.tiff",
-            "CLP.tiff",
-            "L2A_data.tiff",
-            "dataMask.tiff",
-            "sunAzimuthAngles.tiff",
-            "sunZenithAngles.tiff",
-            "viewAzimuthMean.tiff",
-            "viewZenithMean.tiff"
-        ]
 
     def get_data_points(self):
-        hr_data_points_dir = os.listdir(self.data_folder + "/" + self.hr_dataset_name)
-        lr_data_points_dir = os.listdir(self.data_folder + "/" + self.lr_dataset_name)
-        data_points_dir = list(set(hr_data_points_dir + lr_data_points_dir))
-
-        if len(data_points_dir) != len(hr_data_points_dir):
-            raise Exception("Hr and Lr datasets do not match")
-
-        data_points_df = pd.DataFrame()
-        data_points_df['points_name'] = data_points_dir  # Dada que los datos son una combinación de dataframes podría
-        # poner una opción para decidir con que dataframes se trabaja.
+        data_points_df = pd.read_csv(self.data_folder + "/metadata.csv", sep=",")
         return data_points_df
 
-    def get_random_data(self, n_samples):
-        data_names = self.data_points['points_name'].sample(n=n_samples)
+    def get_random_data(self, n_samples = 10, n_revisits=1):
+        data_names = self.data_points['ID'].sample(n=n_samples)
         hr_images = self.get_hr_images(data_names)
-        lr_images = self.get_lr_images(data_names)
+        lr_images = self.get_lr_images(data_names, n_revisits)
         return hr_images, lr_images
 
     def get_hr_images(self, data_points):
@@ -55,16 +35,36 @@ class DataManager:
         images_tensor = torch.stack([transforms.ToTensor()(img) for img in images])
         return images_tensor
 
-    def get_lr_images(self, data_points):
-        images_tensor = torch.zeros((len(data_points)))
-        source = "L2A"  # Con el core es fijo aqui pero en el  futuro debe ser interchangeable
+    def get_lr_images(self, data_points, n_revisits=1, image_shape=(157, 159, 3)):
+        images = torch.zeros(len(data_points), n_revisits, *image_shape)
+        source = "L2A"
         for i, data_point in enumerate(data_points):
-            data_directory = self.data_folder + "/" + self.lr_dataset_name + "/" + data_point + "/" + source + "/"
-            image_id = random.randint(1, 8)
-            parts_list = []
-            for data_part_name in self.lr_data_part_names:
-                directory = data_directory + data_point + "-" + str(image_id) + "-" + source + data_part_name
-                parts_list.append(transforms.ToTensor()(tifffile.imread(directory)))
-            image_tensor = torch.stack(parts_list)
-            images_tensor[i] = image_tensor
-        return images_tensor
+            directory = self.data_folder + "/" + self.lr_dataset_name + "/" + data_point + "/"
+            lr_images_package = self.read_sat_images(directory, n_revisits=n_revisits)
+            images[i] = lr_images_package
+        return images
+
+    def transform_to_rgb(self, sat_image):
+        blue_band = sat_image[3][:, :, 0]
+        green_band = sat_image[3][:, :, 1]
+        red_band = sat_image[3][:, :, 2]
+
+        rgb_image = np.zeros((sat_image[3].shape[0], sat_image[3].shape[1], 3))
+        rgb_image[:, :, 0] = red_band
+        rgb_image[:, :, 1] = green_band
+        rgb_image[:, :, 2] = blue_band
+        return rgb_image
+
+    def read_sat_images(self, folder, image_shape=(157, 159, 3), n_revisits=1):
+        images = torch.zeros(n_revisits, *image_shape)
+        i = 1
+        for file_name in enumerate(os.listdir(folder)):
+            if i > n_revisits:
+                break
+            if file_name.__contains__("_data"):
+                image = tifffile.imread(folder + file_name)
+                images[i] = torch.from_numpy(self.transform_to_rgb(image))
+                i += 1
+        if n_revisits == 1:
+            images.squeeze()
+        return images
