4eedbfe7d1d06e3c06c84e36bc65c27036f28753
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-03-18 17:26:24 +0000

    Modification of lr_images reader. If n_revisits == 1 it squeezes the dimension

--- utils/DataManager.py
+++ utils/DataManager.py
@@ -35,19 +35,25 @@ class DataManager:
         images_tensor = torch.stack([transforms.ToTensor()(img) for img in images])
         return images_tensor
 
-    def get_lr_images(self, data_points, n_revisits=1, image_shape=(157, 159, 3)):
-        images = torch.zeros(len(data_points), n_revisits, *image_shape)
-        source = "L2A"
+    def get_lr_images(self, data_points, n_revisits=1, image_shape=(3, 157, 159)):
+
+        if n_revisits == 1:
+            images = torch.zeros(len(data_points), *image_shape)
+        else:
+            images = torch.zeros(len(data_points), n_revisits, *image_shape)
+
         for i, data_point in enumerate(data_points):
             directory = self.data_folder + "/" + self.lr_dataset_name + "/" + data_point + "/"
             lr_images_package = self.read_sat_images(directory, n_revisits=n_revisits)
+            lr_images_package.squeeze()
             images[i] = lr_images_package
         return images
 
     def transform_to_rgb(self, sat_image):
-        blue_band = sat_image[3][:, :, 0]
-        green_band = sat_image[3][:, :, 1]
-        red_band = sat_image[3][:, :, 2]
+        print(sat_image.shape)
+        blue_band = sat_image[0][:, :, 0]
+        green_band = sat_image[0][:, :, 1]
+        red_band = sat_image[0][:, :, 2]
 
         rgb_image = np.zeros((sat_image[3].shape[0], sat_image[3].shape[1], 3))
         rgb_image[:, :, 0] = red_band
@@ -55,7 +61,7 @@ class DataManager:
         rgb_image[:, :, 2] = blue_band
         return rgb_image
 
-    def read_sat_images(self, folder, image_shape=(157, 159, 3), n_revisits=1):
+    def read_sat_images(self, folder, image_shape=(3, 157, 159), n_revisits=1):
         images = torch.zeros(n_revisits, *image_shape)
         i = 1
         for file_name in enumerate(os.listdir(folder)):
@@ -65,6 +71,4 @@ class DataManager:
                 image = tifffile.imread(folder + file_name)
                 images[i] = torch.from_numpy(self.transform_to_rgb(image))
                 i += 1
-        if n_revisits == 1:
-            images.squeeze()
         return images
