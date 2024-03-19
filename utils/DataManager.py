c6654c1eccb6ebe101005e90b9bff5f0cfb801e4
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-03-19 16:41:06 +0000

    Corrections in reader + modularization

--- utils/DataManager.py
+++ utils/DataManager.py
@@ -1,11 +1,10 @@
-import os
 import pandas as pd
 import torch
 import torchvision.transforms as transforms
 import PIL.Image as Image
 import tifffile
 import numpy as np
-
+import os
 
 class DataManager:
     def __init__(self, data_directory, lr_dataset_name, hr_dataset_name):
@@ -21,7 +20,7 @@ class DataManager:
         data_points_df = pd.read_csv(self.data_folder + "/metadata.csv", sep=",")
         return data_points_df
 
-    def get_random_data(self, n_samples = 10, n_revisits=1):
+    def get_random_data(self, n_samples=10, n_revisits=1):
         data_names = self.data_points['ID'].sample(n=n_samples)
         hr_images = self.get_hr_images(data_names)
         lr_images = self.get_lr_images(data_names, n_revisits)
@@ -35,8 +34,7 @@ class DataManager:
         images_tensor = torch.stack([transforms.ToTensor()(img) for img in images])
         return images_tensor
 
-    def get_lr_images(self, data_points, n_revisits=1, image_shape=(3, 157, 159)):
-
+    def get_lr_images(self, data_points, n_revisits=1, image_shape=(164, 164, 3)):
         if n_revisits == 1:
             images = torch.zeros(len(data_points), *image_shape)
         else:
@@ -44,31 +42,47 @@ class DataManager:
 
         for i, data_point in enumerate(data_points):
             directory = self.data_folder + "/" + self.lr_dataset_name + "/" + data_point + "/"
-            lr_images_package = self.read_sat_images(directory, n_revisits=n_revisits)
+            lr_images_package = self.read_sat_image(directory, image_shape=image_shape, n_revisits=n_revisits)
             lr_images_package.squeeze()
             images[i] = lr_images_package
         return images
 
-    def transform_to_rgb(self, sat_image):
-        print(sat_image.shape)
-        blue_band = sat_image[0][:, :, 0]
-        green_band = sat_image[0][:, :, 1]
-        red_band = sat_image[0][:, :, 2]
+    def read_sat_image(self, folder, image_shape, n_revisits):
+        images = torch.zeros(n_revisits, *image_shape)
+        i = 0
+        folder = folder + "L2A/"
+        for file_name in os.listdir(folder):
+            if i >= n_revisits:
+                break
+            if file_name.__contains__("_data"):
+                image = tifffile.imread(folder + file_name)
+                image_tensor = torch.from_numpy(self.transform_to_rgb(image))
+                images[i] = self.apply_padding(image_tensor, image_shape)
+                i += 1
+        return images
 
-        rgb_image = np.zeros((sat_image[3].shape[0], sat_image[3].shape[1], 3))
+    def transform_to_rgb(self, sat_image):
+        blue_band = sat_image[:, :, 0]
+        green_band = sat_image[:, :, 1]
+        red_band = sat_image[:, :, 2]
+
+        rgb_image = np.zeros((sat_image.shape[0], sat_image.shape[1], 3))
         rgb_image[:, :, 0] = red_band
         rgb_image[:, :, 1] = green_band
         rgb_image[:, :, 2] = blue_band
         return rgb_image
 
-    def read_sat_images(self, folder, image_shape=(3, 157, 159), n_revisits=1):
-        images = torch.zeros(n_revisits, *image_shape)
-        i = 1
-        for file_name in enumerate(os.listdir(folder)):
-            if i > n_revisits:
-                break
-            if file_name.__contains__("_data"):
-                image = tifffile.imread(folder + file_name)
-                images[i] = torch.from_numpy(self.transform_to_rgb(image))
-                i += 1
-        return images
+    def apply_padding(self, image_tensor, image_shape):
+        n, m = image_tensor.shape[:2]
+        on, om = image_shape[:2]
+        pad_n = on - n
+        pad_m = om - m
+
+        pad_top = pad_n // 2
+        pad_bottom = pad_n - pad_top
+        pad_left = pad_m // 2
+        pad_right = pad_m - pad_left
+
+        padded_image_tensor = torch.nn.functional.pad(image_tensor, (0, 0, pad_left, pad_right, pad_top, pad_bottom))
+
+        return padded_image_tensor
